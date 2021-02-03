ed5889ba3da592e1925ccfd001658ed83b9a5a16
Lucas Sousa <lucas103101@gmail.com>
2021-02-03 10:12:35 -0300

    Rename file

---
+++ database/cat_table.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+
+
+metadata = sqlalchemy.MetaData()
+
+
+cats = sqlalchemy.Table(
+    "cats",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("breed", sqlalchemy.String),
+    sqlalchemy.Column("location_of_origin", sqlalchemy.String),
+    sqlalchemy.Column("coat_length", sqlalchemy.String),
+    sqlalchemy.Column("body_type", sqlalchemy.String),
+    sqlalchemy.Column("pattern", sqlalchemy.String),
+)
